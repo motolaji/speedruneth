b4ad96cdc0fe26b28ff6941ca1ea75bacff025c7
Omotolani Jaji <51027008+motolaji@users.noreply.github.com>
2022-04-02 11:55:10 +0100

    Update hardhat.config.js

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -57,7 +57,7 @@ module.exports = {
             url: "https://rinkeby.infura.io/v3/ec6a8acd1d354717acec099ad46a0bab", // <---- YOUR INFURA ID! (or it won't work)
             //    url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/eth/rinkeby", // <---- YOUR MORALIS ID! (not limited to infura)
             accounts: {
-                mnemonic: mnemonic("very much stool fit quick cube include asthma admit lawsuit rival deliver"),
+                mnemonic: mnemonic(""),
             },
         },
         kovan: {
